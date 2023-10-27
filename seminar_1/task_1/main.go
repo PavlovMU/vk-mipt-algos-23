dc6befce620bcaf906f13b29772e84cdeb99173d
Andrey Filkin <andreychannel123123@gmail.com>
2023-10-27 18:03:29 +0300

    first solution

---
+++ seminar_1/task_1/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type LinkedList struct {
+	Lst []ListNode
+}
+type ListNode struct {
+	Value int
+	Next  *ListNode
+}
+
+func reverseList(lst *ListNode) *ListNode {
+	if lst == nil {
+		return nil
+	}
+
+	var reverseLst *ListNode
+
+	for lst != nil {
+		tmp := lst.Next
+		lst.Next = reverseLst
+		reverseLst = lst
+		lst = tmp
+	}
+	return reverseLst
+}
+
+func main() {
+	Lst := make([]ListNode, 3)
+	Lst[2] = ListNode{
+		Value: 2,
+		Next:  nil,
+	}
+	Lst[1] = ListNode{
+		Value: 1,
+		Next:  &Lst[2],
+	}
+	Lst[0] = ListNode{
+		Value: 0,
+		Next:  &Lst[1],
+	}
+	fmt.Println(Lst)
+	reverseList(&Lst[0])
+	fmt.Println(Lst)
+
+}
