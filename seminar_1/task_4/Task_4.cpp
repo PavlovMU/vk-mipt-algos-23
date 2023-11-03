f2be80831604c4abef36f955a6c2013887a559a7
RaILIsH <73170673+RaILIsH@users.noreply.github.com>
2023-11-03 23:30:45 +0300

    Task_4.cpp

--- seminar_1/task_4/Task_4.cpp
+++ seminar_1/task_4/Task_4.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main()
 {
+    //На семинаре говорили, что массив изначально отсортирован.
     vector <int> a{1, 2, 3, 4, 6, 9, 10};
-    int p1 = 0, p2 = a.size() - 1, sum = 8;
-    while (a[p1] + a[p2] != sum) {
-        if (a[p1] + a[p2] > sum) {
+    //sort(a.begin(), a.end());
+    int p1 = 0, p2 = a.size() - 1, summ = 8;
+    while (a[p1] + a[p2] != summ) {
+        if (a[p1] + a[p2] > summ) {
             p2--;
         }
         else{
