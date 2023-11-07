481ba493c09b6d5156bddefe67c5d66ea9c696b9
LynovK <90971447+LynovK@users.noreply.github.com>
2023-11-07 22:53:22 +0300

    Create reverse_ll.py
    решил задачу 2

---
+++ seminar_1/task_2/lynov.nikolai/reverse_ll.py
@@ -0,0 +1,10 @@
+def reverseList(head):
+    new_list = None
+    current = head
+
+    while current:
+        next_node = current.next
+        current.next = new_list
+        new_list = current
+        current = next_node
+    return new_list
