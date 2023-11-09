01eac57c2cd646ff3f123e37fcb0a65a442d6d3a
Shukrullo Jumanazarov <shukrullo.coder@gmail.com>
2023-11-09 17:42:38 +0300

    first task is done

---
+++ seminar_2/task_1/jumanazarov.shukrullo/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main() {
+	std::string str1;
+	std::string str2;
+	std::cin >> str1 >> str2;
+	int i = 0, j = 0;
+	int n = str1.length(), m = str2.length();
+	while (i < n && j < m) {
+		if (str1[i] == str2[j]) {
+			i++;
+		}
+		j++;
+	}
+	std::cout << (i == n ? "yes\n" : "no\n");
+}
