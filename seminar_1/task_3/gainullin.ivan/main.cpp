e7b14f9071cb26004851092969e02d86eda769f8
BitmanBT <70342539+BitmanBT@users.noreply.github.com>
2023-11-09 10:59:35 +0300

    gainullin ivan
    * Решил задачи с первого семинара
    
    * Привел в порядок файловую структуру
    
    ---------
    
    Co-authored-by: BitmanBT <gainullin.ii@phystech.edu>

---
+++ seminar_1/task_3/gainullin.ivan/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+struct Node {
+    Node() : data(0), next(nullptr) {}
+    Node(int data_) : data(data_), next(nullptr) {}
+    Node(int data_, Node* next_) : data(data_), next(next_) {}
+    Node(const Node* node) : data(node->data), next(node->next) {}
+
+    int data;
+    Node* next;
+};
+
+std::ostream& operator<<(std::ostream& os, const Node& node) {
+    os << node.data;
+    return os;
+}
+
+struct LinkedList {
+    LinkedList() : head(nullptr), size(0) {}
+    LinkedList(Node* head_, size_t size_) : head(head_), size(size_) {}
+
+    Node* head;
+    size_t size;
+};
+
+std::ostream& operator<<(std::ostream& os, const LinkedList& list) {
+    Node* tmp(new Node(list.head->data, list.head->next));
+    while (tmp->next != nullptr) {
+        os << tmp->data << ' ';
+        tmp = tmp->next;
+    }
+    os << tmp->data;
+    os << std::endl;
+    return os;
+}
+
+bool hasCycle(Node* head) {
+    if (head->next == nullptr || head->next->next == nullptr)
+        return false;
+    Node* slow(head);
+    Node* fast(head->next);
+    while (slow != fast) {
+        if (fast == nullptr)
+            return false;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return true;
+}
+
+int main() {
+    std::vector<Node> a(5);
+    for (size_t i = 0; i < a.size()-1; i++) {
+        a[i].data = i;
+        a[i].next = &a[i+1];
+    }
+    a[a.size()-1].data = a.size()-1;
+
+    LinkedList list(&a[0], a.size());
+    std::cout << "List has cycle: " << hasCycle(list.head) << std::endl;
+
+    a[a.size()-1].next = &a[a.size()-3];
+    std::cout << "List has cycle: " << hasCycle(list.head) << std::endl;
+}
