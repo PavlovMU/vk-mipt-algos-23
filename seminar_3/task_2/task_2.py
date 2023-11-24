2fff49bbe1ada35cda7d9a4fa3c1ec9bffd14853
Islam Khamitov <khamitov.ir@phystech.edu>
2023-11-24 15:44:25 +0300

    Решил вторую задачу

--- seminar_3/task_2/task_2.py
+++ seminar_3/task_2/task_2.py
@@ -1,27 +1,40 @@
-class HashTable:
-    def __init__(self):
-        self.table = {}
-
-    def contains(self, key):
-        return key in self.table
-
-    def insert(self, key, value):
-        self.table[key] = value
-
-    def get(self, key):
-        return self.table.get(key)
-
-
-def twoSum(data, target):
-    ht = HashTable()
-    for i in range(len(data)):
-        if ht.contains(data[i]): # среди ключей нашей таблицы ищем data[i]
-            return [i, ht.table[data[i]]]
-        diff = target - data[i]
-        ht.table[diff] = i
-    return []
+def quicksort(nums):
+   if len(nums) <= 1:
+       return nums
+   else:
+       q = nums[len(nums) // 2] 
+       s_nums = [] # меньше q
+       g_nums = [] # больше q
+       e_nums = [] # равные q
+       for n in nums:
+           if n < q:
+               s_nums.append(n)
+           elif n > q:
+               g_nums.append(n)
+           else:
+               e_nums.append(n)
+       return quicksort(s_nums) + e_nums + quicksort(g_nums)
 
 
-a = [6, 4,3,2,8,5]
-target = 11
-print(twoSum(a, target))
+#задача 2 накормить животных
+
+def feedAnimals(animals, food):
+    if len(animals) == 0 or len(food) == 0:
+        return 0
+    quicksort(animals)
+    quicksort(food)
+    
+    count = 0 
+    for f in food:
+        if f >= animals[count]:
+            count += 1
+        
+        if count == len(animals):
+            break
+        
+        
+    return count    
+
+animals = [2, 1, 4, 9]
+food = [3, 2, 5, 7]
+print(feedAnimals(animals, food))
