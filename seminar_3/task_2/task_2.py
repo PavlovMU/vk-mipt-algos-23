1f0ec2c370f7fef89dad0222c7cb9b7faf198098
Islam Khamitov <khamitov.ir@phystech.edu>
2023-11-24 15:31:38 +0300

    Решил вторую задачу

---
+++ seminar_3/task_2/task_2.py
@@ -0,0 +1,27 @@
+class HashTable:
+    def __init__(self):
+        self.table = {}
+
+    def contains(self, key):
+        return key in self.table
+
+    def insert(self, key, value):
+        self.table[key] = value
+
+    def get(self, key):
+        return self.table.get(key)
+
+
+def twoSum(data, target):
+    ht = HashTable()
+    for i in range(len(data)):
+        if ht.contains(data[i]): # среди ключей нашей таблицы ищем data[i]
+            return [i, ht.table[data[i]]]
+        diff = target - data[i]
+        ht.table[diff] = i
+    return []
+
+
+a = [6, 4,3,2,8,5]
+target = 11
+print(twoSum(a, target))
